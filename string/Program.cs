ee79c111e47e6322a11ee2b3473d69f3e1380bdd
MarceloBertochi <paidasamia@gmail.com>
2023-04-18 12:16:30 -0300

    estudos da w3schools

---
+++ string/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EstudodeString
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            //Crie uma variável do tipo string e atribua um valor a ela:
+            string nome = "Marcelo tirou nota MB em programação na Etec";
+
+            Console.WriteLine(nome.ToUpper());
+            Console.WriteLine(nome.ToLower());
+            Console.WriteLine("A variável nome tem entre letras e espaços " + nome.Length + " caracteres");
+        }
+    }
+
+}
+
+
+
+
