84a637e8b668ee220cc70bb12ffd770aed06cda8
MarceloBertochi <paidasamia@gmail.com>
2023-04-18 15:10:48 -0300

    estudo da w3school

---
+++ interpolacao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Concatenação
+
+{
+    class Program
+ 
+    {
+        static void Main(string[] args)
+
+        {
+           string nome = "Marcelo", sobrenome = "Bertochi";
+
+           
+           string completo = $"Meu nome completo é {nome} {sobrenome}";
+
+           Console.WriteLine(completo);
+
+           
+           Console.WriteLine($"Meu nome completo é {nome} {sobrenome}");
+
+
+
+        }
+    }
+}
