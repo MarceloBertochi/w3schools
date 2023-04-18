6640b59844fbbdb6f1ac047e63a48976a68e835e
MarceloBertochi <paidasamia@gmail.com>
2023-04-18 14:44:48 -0300

    Estudos da w3schools

---
+++ concatenacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Concatenação
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            string nome = "Marcelo ", sobrenome = "Bertochi";
+
+
+            Console.WriteLine(nome + sobrenome);
+
+            string completo = string.Concat(nome, sobrenome);
+
+            // int x = 10, y = 20;
+            //Console.WriteLine(x + y);
+
+            string x = "10", y = "20";
+            Console.WriteLine(x + y);
+
+        }
+    }
+}
+
+
