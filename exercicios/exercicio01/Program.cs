506e8bbfa2e3c882aaac41fa276b4b1ce76df385
MarceloBertochi <paidasamia@gmail.com>
2023-04-18 11:40:03 -0300

    estudo do w3sachools

---
+++ exercicios/exercicio01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio01 // Escreva em C# um programa que de um print Hello e seu nome em linha separada.
+
+{
+   class programa
+
+    {
+        static void Main()
+
+        {
+          Console.WriteLine("Hello");
+           Console.WriteLine("Marcelo");
+        }
+    }
+}
---
+++ exercicios/exercicio02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercicio02 // Escreva um programa que printa o soma de dois numeros. 
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            System.Console.WriteLine(4 + 6);
+        }
+    }
+}
