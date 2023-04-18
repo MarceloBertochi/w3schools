a4dbf4471d77ce478a28e96b8ff289d3dfe41a0c
MarceloBertochi <paidasamia@gmail.com>
2023-04-18 11:34:09 -0300

    Estudo do w3schools

---
+++ math/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConfiguraçõesMatemáticas
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            int x = 150, y = -200;
+
+            Console.WriteLine(Math.Max(x, y)); // retorna o maior valor
+            Console.WriteLine(Math.Min(x, y)); // retorna o menor valor
+            Console.WriteLine(Math.Sqrt(x + y)); // retorna a raiz quadrada de x + y 
+            Console.WriteLine(Math.Abs(x + y)); // retorna o valor absoluto de x + y
+            Console.WriteLine(Math.Round(9.99));
+        }
+ 
+    }
+}
