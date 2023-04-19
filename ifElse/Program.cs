672ea12518a1840c7615765440a180a7c1d1e332
MarceloBertochi <paidasamia@gmail.com>
2023-04-19 15:28:28 -0300

    estudo da w3school

---
+++ ifElse/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace IfElse
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            /*
+             if (20 > 18)
+             {
+                 Console.WriteLine("20 é maior que 18");
+             }
+             */
+            /*
+            int x = 20;
+            int y = 18;
+            if (x > y)
+            {
+                Console.WriteLine("x é maior que y");
+            }
+            */
+
+            /*
+             int time = 20;
+             if (time < 18)
+             {
+                 Console.WriteLine("Bom dia.");
+             }
+             else
+             {
+                 Console.WriteLine("Boa noite.");
+             }
+             */
+
+            int time = 22;
+            if (time < 10)
+            {
+                Console.WriteLine("Bom dia.");
+            }
+            else if (time < 20)
+            {
+                Console.WriteLine("Boa tarde.");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite.");
+            }
+
+
+
+        }
+    }
+}
