0c7ce1eb4d28e0b46f5a235441a921ef7c217217
MarceloBertochi <paidasamia@gmail.com>
2023-05-23 20:05:50 -0300

    estudo do Loop while e do

---
+++ wile/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+sintaxe do wile
+
+wile (condição)
+{
+    bloco de código a ser executado
+}
+*/
+
+
+/* 
+int i = 0;
+while (i < 5) 
+{
+  Console.WriteLine(i);
+  i++;
+}
+*/
+
+/*
+do 
+{
+  // code block to be executed
+}
+while (condition);
+*/
+
+int i = 0;
+
+do
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+while (i <= 5);
